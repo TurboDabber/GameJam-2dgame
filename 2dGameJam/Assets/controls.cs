e7b99b1ed42e9b1b7d45fb68ea10a863ec78c2b8
CashmereOgre <43883388+CashmereOgre@users.noreply.github.com>
2020-04-03 20:32:23 +0200

    Sterowanie bez animacji

---
+++ 2dGameJam/Assets/controls.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controls : MonoBehaviour
+{
+    public float movement_speed;
+    public Rigidbody2D rigidbody;
+    Vector2 movement;
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + movement * movement_speed * Time.fixedDeltaTime);
+    }
+}
