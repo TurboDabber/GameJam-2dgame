7f9c3bdfba2beb1cee3ba70659c4218bb33fd11b
PawelLutostanski <lucio.lut@gmail.com>
2020-04-04 01:30:57 +0200

    Finally procedural generation
    please change an REPETITIONS to set number of generated tiles

---
+++ 2dGameJam/Assets/Generate/instantiateTiles.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instantiateTiles : MonoBehaviour
+{
+    public GameObject[] myPrefab;
+    private int random;
+    public int REPETITIONS = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 pos_vec=new Vector3( 0.0f, 0.0f, 0.0f );
+        for (int i = 0; i < REPETITIONS; i++)
+        {
+            random = Random.Range(0, myPrefab.Length);
+            Instantiate(myPrefab[random], pos_vec, Quaternion.identity);
+            random = Random.Range(0, 4);
+            if(random==0)
+            {
+                pos_vec.x += 1;
+            }
+            else if(random==1)
+            {
+                pos_vec.x += -1;
+            }
+            else if(random == 2)
+            {
+                pos_vec.y += 1;
+            }
+            else if(random == 3)
+            {
+                pos_vec.y -= 1;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
