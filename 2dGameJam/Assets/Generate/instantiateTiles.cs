bdeaed76e65dd024ec8efadd5064b68eaed5e386
PawelLutostanski <lucio.lut@gmail.com>
2020-04-04 17:28:58 +0200

    Update instantiateTiles.cs

--- 2dGameJam/Assets/Generate/instantiateTiles.cs
+++ 2dGameJam/Assets/Generate/instantiateTiles.cs
@@ -6,14 +6,40 @@ public class instantiateTiles : MonoBehaviour
 {
     public GameObject[] myGrounds;
     public GameObject[] myWalls;
+    const int width=300;
+    const int height=300;
+    public  short[,] game_board = new short[height, width];
     private int random;
     public int REPETITIONS = 20;
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 pos_vec=new Vector3( 0.0f, 0.0f, 0.0f );
+        Board_init();
+        Vector3 pos_vec=new Vector3(-width/2, -height/2, 0.0f );
+        for(int i=1;i<height-1;i++)
+            for(int j=1;j<width-1;j++)
+            {
+                Vector3 buff_vec = pos_vec;
+                if (game_board[i,j]==1)
+                {
+                    
+                    buff_vec.x += j;
+                    buff_vec.y += i;
+                    random = Random.Range(0, myGrounds.Length);
+                    Instantiate(myGrounds[random], pos_vec, Quaternion.identity);
+                }
+                if(game_board[i,j]==2)
+                {
+                    buff_vec.x += j;
+                    buff_vec.y += i;
+                    random = Random.Range(0, myWalls.Length);
+                    Instantiate(myWalls[random], pos_vec, Quaternion.identity);
+                }
+            }
+        /*
         for (int i = 0; i < REPETITIONS; i++)
         {
+
             random = Random.Range(0, myGrounds.Length);
             Instantiate(myGrounds[random], pos_vec, Quaternion.identity);
             random = Random.Range(0, 4);
@@ -33,7 +59,61 @@ public class instantiateTiles : MonoBehaviour
             {
                 pos_vec.y -= 1;
             }
-        }
+        }*/
+    }
+
+    void Board_init()
+    {
+        int a = height / 2, b = width / 2;
+        for(int i=a-2;i<a+2;i++)
+            for(int j=b-2;j<b+2;j++)
+            {
+                game_board[i, j] = 1;
+            }
+        for (int i = 2; i < height-2; i++)
+            for (int j = 2; j < width-2; j++)
+            {
+               
+                random =Random.Range(0,4);
+
+            }
+
+
+        for (int i = 0; i < height; i++)//makes frame from walls
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (game_board[i, j] == 0)
+                {
+                    if (i > 0 && i < (height - 1))
+                    {
+                        if (game_board[i - 1, j] == 1)
+                        {
+                            game_board[i - 1, j] = 2;
+                        }
+                        if (game_board[i + 1, j] == 1)
+                        {
+                            game_board[i + 1, j] = 2;
+                        }
+
+                    }
+                    if (j > 0 && j < (height - 1))
+                    {
+                        if (game_board[i, j + 1] == 1)
+                        {
+                            game_board[i, j + 1] = 2;
+                        }
+                        if (game_board[i, j + 1] == 1)
+                        {
+                            game_board[i, j + 1] = 2;
+                        }
+
+                    }
+
+                }
+            }
+
+         }
     }
 
     // Update is called once per frame
