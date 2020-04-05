f308a5688aaea075260319dbd839a1649ed71010
PawelLutostanski <lucio.lut@gmail.com>
2020-04-05 17:08:23 +0200

    bar

---
+++ 2dGameJam/Assets/Health.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public Slider slider;
+    // Start is calledre the first frame update
+    public void setMax(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+
+    public void set_health(int health)
+    {
+        slider.value = health;
+    }
+}
--- 2dGameJam/Assets/Parameters.cs
+++ 2dGameJam/Assets/Parameters.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Parameters : MonoBehaviour
 {
+    public const int health_max = 100;
+    public int health_now = 100;
+    public int collected_frames = 0;
+    public int all_frames = 4;
+    public Health healthBar;/// UI bar 
     // Start is called before the first frame update
     void Start()
     {
-        
+        health_now = health_max;
+        healthBar.setMax ( health_max);//////////setting max health
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))/////////testing
+        {
+            TakeDmg(20);
+        }
+    }
+    void TakeDmg(int dmg_given)
+    {
+        health_now -= dmg_given;
+        healthBar.set_health(health_now);
     }
 }
