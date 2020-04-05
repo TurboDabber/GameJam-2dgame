678d943a2d03ba6cf3cd583349829fbdaa352c34
PawelLutostanski <lucio.lut@gmail.com>
2020-04-06 00:10:15 +0200

    dużo rzeczy

--- 2dGameJam/Assets/Parameters.cs
+++ 2dGameJam/Assets/Parameters.cs
@@ -11,12 +11,13 @@ public class Parameters : MonoBehaviour
     bool fail = false;
     public Health healthBar;/// UI bar 
     public CollectedFrames collected;
-    GameObject Ending;
+    public GameObject Ending;
     // Start is called before the first frame update
     void Start()
     {
         health_now = health_max;
-        healthBar.setMax ( health_max);//////////setting max health
+        healthBar.setMax(health_max);//////////setting max health
+        collected_frames = 3;
         collected.fillAmount = collected_frames;
         collected.maxAmount = all_frames;
         collected.setText(0);
@@ -26,20 +27,21 @@ public class Parameters : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))/////////testing
+        if (Input.GetKeyDown(KeyCode.Space))/////////testing
         {
             TakeDmg(20);
-            
+
         }
-        
+
     }
     void TakeDmg(int dmg_given)
     {
         health_now -= dmg_given;
-        if(health_now<0)
+        if (health_now < 0)
         {
             fail = true;
         }
+
         healthBar.set_health(health_now);
     }
     void AddFrames()
@@ -51,17 +53,18 @@ public class Parameters : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag=="Frame")
+        if (collision.gameObject.tag == "Frame")
         {
-            
-            
+
+
             AddFrames();
-            if (collected_frames == 4)
+            if (collected_frames >= 4)
             {
-                Vector3 SpawnHere = new Vector3(collision.transform.position.x, collision.transform.position.y,0.0f);
+                Vector3 SpawnHere = new Vector3(collision.transform.position.x, collision.transform.position.y, 0.0f);
                 Instantiate(Ending, SpawnHere, Quaternion.identity);
             }
             Destroy(collision.gameObject);
         }
+
     }
 }
