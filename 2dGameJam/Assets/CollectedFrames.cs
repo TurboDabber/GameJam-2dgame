7a554bdc686ff54b70ea207a4f96a365da83a58e
PawelLutostanski <lucio.lut@gmail.com>
2020-04-05 18:28:16 +0200

    parameters update

---
+++ 2dGameJam/Assets/CollectedFrames.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectedFrames : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int fillAmount;
+    public int maxAmount;
+
+    [SerializeField]
+    public Text information;
+
+   public void setText()
+    {
+        string Temp = fillAmount + " / " + maxAmount;
+        information.text=Temp;
+    }
+}
--- 2dGameJam/Assets/Parameters.cs
+++ 2dGameJam/Assets/Parameters.cs
@@ -7,14 +7,19 @@ public class Parameters : MonoBehaviour
     public const int health_max = 100;
     public int health_now = 100;
     public int collected_frames = 0;
-    public int all_frames = 4;
+    public const int all_frames = 4;
+    bool fail = false;
     public Health healthBar;/// UI bar 
+    public CollectedFrames collected;
     // Start is called before the first frame update
     void Start()
     {
         health_now = health_max;
         healthBar.setMax ( health_max);//////////setting max health
-    }
+        collected.fillAmount = collected_frames;
+        collected.maxAmount = all_frames;
+    }/// <summary>
+    /// </summary>
 
     // Update is called once per frame
     void Update()
@@ -27,6 +32,16 @@ public class Parameters : MonoBehaviour
     void TakeDmg(int dmg_given)
     {
         health_now -= dmg_given;
+        if(health_now<0)
+        {
+            fail = true;
+        }
         healthBar.set_health(health_now);
+    }
+    void AddFrames()
+    {
+        collected.fillAmount = ++collected_frames;
+        collected.setText();
+
     }
 }
